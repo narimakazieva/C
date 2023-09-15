eb1f06a33c0c3d591f830a9ac0ddaa643bf03ac0
Narima <narimakazieva2@gmail.com>
2023-09-15 15:04:49 +0300

    задача 3

---
+++ taskt3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: "):
+int num = int.Parse(Console.ReadLine());
+int remain = num % 2;
+if (remain == 0)
+{
+    Console.WriteLine("Число "+ num +" четное?");
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Число "+ num +" четное?");
+    Console.WriteLine("Нет");
+}
