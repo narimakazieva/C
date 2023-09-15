c7daa2f5bc00098b7d44779db9547e1620c2cfa4
Narima <narimakazieva2@gmail.com>
2023-09-15 15:08:19 +0300

    исправила ошибку

--- taskt3/Program.cs
+++ taskt3/Program.cs
@@ -1,13 +1,13 @@
-﻿Console.Write("Введите число: "):
+﻿Console.Write("Введите число: ");
 int num = int.Parse(Console.ReadLine());
 int remain = num % 2;
 if (remain == 0)
 {
-    Console.WriteLine("Число "+ num +" четное?");
-    Console.WriteLine("Да");
+    Console.WriteLine("Число " + num + " четное?");
+    Console.WriteLine("ДА!");
 }
 else
 {
-    Console.WriteLine("Число "+ num +" четное?");
-    Console.WriteLine("Нет");
+    Console.WriteLine("Число " + num + " четное?");
+    Console.WriteLine("НЕТ!");
 }
